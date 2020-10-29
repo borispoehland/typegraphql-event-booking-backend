7563603d032f3ded59e466166864efc5928878ee
Boris Pöhland <boris.poehland@gmail.com>
2020-10-29 14:31:50 +0100

    removed emitSchemaFile

--- dist/index.js
+++ dist/index.js
@@ -63,7 +63,6 @@ function bootstrap() {
                 case 1:
                     _a.sent();
                     return [4 /*yield*/, type_graphql_1.buildSchema({
-                            emitSchemaFile: true,
                             resolvers: [UserResolver_1.UserResolver, EventResolver_1.EventResolver, BookingResolver_1.BookingResolver],
                             authChecker: auth_checker_1.default,
                         })];
